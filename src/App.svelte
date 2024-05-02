7a6a0190e909ebdea207ffa6ed763bd2c45a0158
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2024-05-02 15:31:33 +0200

    Add table component

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,41 @@
   import Navbar from "./components/Navbar.svelte";
   import Sidebar from "./components/Sidebar.svelte";
   import Button from "./components/Button.svelte";
+  import Table from "./components/Table.svelte";
 
-  function handleButtonClick(){
+  /* Function for button */
+  function handleButtonClick() {
     console.log("handleButtonClick()");
   }
+
+  /* Data for table */
+  let tableData = `  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>`;
 </script>
 
 <Navbar />
@@ -15,4 +46,8 @@
   <h1>Hello world</h1>
   <p>Press the following button to get started</p>
   <Button onButtonClick={handleButtonClick} buttonText={"Get started"} />
+
+  <div class="mt-5">
+    <Table data={tableData} />
+  </div>
 </div>
---
+++ src/components/Table.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let data;
+</script>
+
+<table class="table table-hover">
+  {@html data}
+</table>
