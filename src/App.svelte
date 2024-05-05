8917413a635729a1b925d283788e2a82d4414427
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2024-05-05 23:20:12 +0200

    Add card

--- src/App.svelte
+++ src/App.svelte
@@ -63,23 +63,68 @@
   <h1>Hello world</h1>
   <p>Press the following button to get started</p>
 
-  <!-- Button -->
-
   <Button onButtonClick={handleButtonClick} buttonText={"Get started"} />
 
-  <!-- Table -->
   <div class="mt-5">
     <Table data={tableData} />
   </div>
 
-  <div class="col-6 offset-3"><LoginForm /></div>
+  <!-- <div class="col-6 offset-3"><LoginForm /></div> -->
 
-  <!-- Button trigger modal -->
   <div class="mt-5">
     <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
       Launch demo modal
     </button>
     <Modal modalType={"add-user"} />
+  </div>
+
+  <br />
+  <br />
+
+  <div class="card-1 border rounded">
+    <div class="container">
+      <div class="row">
+        <div class="col mt-3">
+          <div
+            class="border rounded d-flex justify-content-center align-items-center"
+            style="height: 30px; width: 30px;"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-person"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"
+              />
+            </svg>
+          </div>
+        </div>
+        <div class="col mt-3 d-flex justify-content-end">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-three-dots-vertical"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div class="mt-2">
+        <p class="fw-light">customer</p>
+        <p class="fs-3 fw-bold">2,240</p>
+      </div>
+    </div>
   </div>
 </div>
 
@@ -88,7 +133,14 @@
 <!-- <Sidebar /> -->
 
 <!-- <div class="col-6 mt-5">
-  <Select bind:type />
+    <Select bind:type />
 
-  <Chart {config} />
-</div> -->
+    <Chart {config} />
+  </div> -->
+
+<style>
+  .card-1 {
+    height: 160px;
+    width: 250px;
+  }
+</style>
